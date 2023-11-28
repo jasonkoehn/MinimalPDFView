db831a18c44c32b0ed52e7cd3dc9585d689e4344
jasonkoehn <93397575+jasonkoehn@users.noreply.github.com>
2023-11-28 10:02:09 -0700

    Initial Commit

---
+++ MinimalPDFView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MinimalPDFView
+//
+//  Created by Jason Koehn on 11/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MinimalPDFView/MinimalPDFViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalPDFViewApp.swift
+//  MinimalPDFView
+//
+//  Created by Jason Koehn on 11/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MinimalPDFViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
